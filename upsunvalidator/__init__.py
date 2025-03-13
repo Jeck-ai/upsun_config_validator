f765fa93437dc5eee917f7e0852ac1da24b03fe2
Robert Douglass <rob@robshouse.net>
2025-03-13 11:15:29 +0100

    Set up to automatically update version

--- upsunvalidator/__init__.py
+++ upsunvalidator/__init__.py
@@ -1,6 +1,9 @@
 """Validation library for Upsun Configuration."""
 
-__version__ = '0.2.3'
+try:
+    from upsunvalidator._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0.dev0"  # Fallback version if not installed from git
 
 from upsunvalidator.__main__ import validate, validate_string
 from upsunvalidator.templates import (
